43a2e95375848934c14984a824210c3352820955
Mostafa Negm <mostafa.negm.fcis@gmail.com>
2018-04-08 01:37:36 +0200

    Fail in Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -30,16 +30,8 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-preload_app!
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+#preload_app!
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/
-  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
 # are forked to prevent connection leakage.
